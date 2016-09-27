a4929be6cecd32144c381eed6672f6e79c04f248
piyo56 <piyo56.net@gmail.com>
2016-09-27 22:28:00 +0900

    [Add] add even_child_can_understand contents

---
+++ even_child_can_understand/mitakalab-twitter/app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find_by(:username => params[:username])
+    #@user = Hash.new
+    #@user[:name] = 'Shohei Aoki'
+    #@user[:username] = 'moyahima'
+    #@user[:location] = 'Tottori, Japan'
+    #@user[:about] = 'Nice to meet you.'
+  end
+end
