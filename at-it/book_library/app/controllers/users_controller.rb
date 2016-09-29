c283e6f68686d5721b56736a864f1b7d3ef4e272
piyo56 <piyo56.net@gmail.com>
2016-09-29 21:39:59 +0900

    [Update](at-it) 特定データに関するscaffoldアクションの実装＆基礎的なリファクタリング手法

--- at-it/book_library/app/controllers/users_controller.rb
+++ at-it/book_library/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: %w(show edit update destroy)
 
   def index
     @users = User.all
@@ -11,15 +13,39 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to "/users"
+      redirect_to users_path
     else
-      render "/users/new"
+      render new_user_path
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render "edit"
+      #redirect_to edit_user_path(@user) #違いを理解
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :department)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id]) # find(val)と同じ
   end
 end
