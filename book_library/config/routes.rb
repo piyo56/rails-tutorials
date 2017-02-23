c705157bc2a08497facd341b2dda113f7e516b7f
piyo56 <piyo56.net@gmail.com>
2017-02-23 10:08:57 +0900

    made index new create operations of User

--- book_library/config/routes.rb
+++ book_library/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: %i(index new create)
 end
--- book_library/db/schema.rb
+++ book_library/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222151157) do
+ActiveRecord::Schema.define(version: 20170223002618) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170222151157) do
     t.text     "outline",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "department"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
